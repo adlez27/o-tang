45faaf5a3de51fddbc2cc601738612425031f648
TheWildVortex <benedict.tamayo@obf.ateneo.edu>
2021-11-26 19:56:28 +0800

    Created Assigned Activities
    ActiveTransactionsActivity
    HistoryActivity
    ViewTransactionActivity
    ViewHistoryTransactionActivity
    EditTransactionActivity

---
+++ app/src/main/java/com/example/o_tang/ViewHistoryTransactionActivity.java
@@ -0,0 +1,14 @@
+package com.example.o_tang;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ViewHistoryTransactionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_history_transaction);
+    }
+}
