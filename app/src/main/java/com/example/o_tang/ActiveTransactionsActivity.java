9d5ff57d57efc50a46567d64a3edff4162e7a886
TheWildVortex <benedict.tamayo@obf.ateneo.edu>
2021-12-13 14:32:08 +0800

    Active Transactions Recycler View Configured
    Transactions based on the uuid stores in the Shared Preferences should now be displayed (May need testing with the create transactions functionality)

--- app/src/main/java/com/example/o_tang/ActiveTransactionsActivity.java
+++ app/src/main/java/com/example/o_tang/ActiveTransactionsActivity.java
@@ -1,14 +1,60 @@
 package com.example.o_tang;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
+import org.androidannotations.annotations.AfterViews;
+import org.androidannotations.annotations.EActivity;
+import org.androidannotations.annotations.ViewById;
+
+import io.realm.Realm;
+import io.realm.RealmResults;
+
+@EActivity(R.layout.activity_active_transactions)
 public class ActiveTransactionsActivity extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_active_transactions);
+    @ViewById(R.id.activeFilterDebt)
+    Button filterDebt;
+    @ViewById(R.id.activeFilterReceive)
+    Button filterReceive;
+    @ViewById(R.id.activeFilterAll)
+    Button filterAll;
+    @ViewById(R.id.activeTransactionsRecycler)
+    RecyclerView transactionsRecycler;
+    @ViewById(R.id.debtTotal)
+    TextView debtTotal;
+    @ViewById(R.id.receiveTotal)
+    TextView receiveTotal;
+
+    Realm realm;
+    SharedPreferences prefs;
+
+    @AfterViews
+    public void init() {
+        realm = Realm.getDefaultInstance();
+        prefs = getSharedPreferences("o-tang", MODE_PRIVATE);
+
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        layoutManager.setOrientation(RecyclerView.VERTICAL);
+        transactionsRecycler.setLayoutManager(layoutManager);
+
+        String uuid = prefs.getString("uuid", "");
+        RealmResults<Transaction> list = realm.where(Transaction.class)
+                .contains("Transactions.userId", uuid)
+                .findAll();
+
+        TransactionAdapter adapter = new TransactionAdapter(this, list, true);
+        transactionsRecycler.setAdapter(adapter);
+    }
+
+    public void onDestroy() {
+        super.onDestroy();
+        realm.close();
     }
 }
--- app/src/main/java/com/example/o_tang/MainActivity.java
+++ app/src/main/java/com/example/o_tang/MainActivity.java
@@ -1,11 +1,44 @@
 package com.example.o_tang;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.widget.Button;
 
+import org.androidannotations.annotations.AfterViews;
+import org.androidannotations.annotations.Click;
 import org.androidannotations.annotations.EActivity;
+import org.androidannotations.annotations.ViewById;
+
+import io.realm.Realm;
+import io.realm.RealmResults;
 
 @EActivity(R.layout.activity_main)
 public class MainActivity extends AppCompatActivity {
+
+    @ViewById(R.id.mainActiveButton)
+    Button activeButton;
+
+    Realm realm;
+    SharedPreferences prefs;
+
+    @AfterViews
+    public void init() {
+        realm = Realm.getDefaultInstance();
+        prefs = getSharedPreferences("o-tang", MODE_PRIVATE);
+    }
+
+    public void onDestroy() {
+        super.onDestroy();
+        realm.close();
+    }
+
+    @Click(R.id.mainActiveButton)
+    public void viewActives()
+    {
+        ActiveTransactionsActivity_.intent(this).start();
+    }
 }
