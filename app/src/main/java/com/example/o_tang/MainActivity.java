ec2dd36e23c108452284dc19cf9f072a528bcd6e
Mark Beltran <markmbeltran@gmail.com>
2021-12-15 15:29:58 +0800

    Display username and totals in main screen

--- app/src/main/java/com/example/o_tang/MainActivity.java
+++ app/src/main/java/com/example/o_tang/MainActivity.java
@@ -7,6 +7,7 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.TextView;
 
 import org.androidannotations.annotations.AfterViews;
 import org.androidannotations.annotations.Click;
@@ -18,9 +19,12 @@ import io.realm.RealmResults;
 
 @EActivity(R.layout.activity_main)
 public class MainActivity extends AppCompatActivity {
-
-    @ViewById(R.id.mainActiveButton)
-    Button activeButton;
+    @ViewById(R.id.mainPerson)
+    TextView name;
+    @ViewById(R.id.mainTotalReceive)
+    TextView receiveTotalText;
+    @ViewById(R.id.mainTotalDebt)
+    TextView debtTotalText;
 
     Realm realm;
     SharedPreferences prefs;
@@ -29,6 +33,29 @@ public class MainActivity extends AppCompatActivity {
     public void init() {
         realm = Realm.getDefaultInstance();
         prefs = getSharedPreferences("o-tang", MODE_PRIVATE);
+
+        String userId = prefs.getString("uuid", "");
+        User user = realm.where(User.class)
+                .equalTo("uuid", userId)
+                .findFirst();
+
+        name.setText(user.getUsername());
+
+        RealmResults<Transaction> userTransactions = realm.where(Transaction.class)
+                .equalTo("userId", userId)
+                .findAll();
+
+        double receiveTotal = 0.0;
+        double debtTotal = 0.0;
+        for(Transaction t : userTransactions) {
+            if (t.isOwed())
+                debtTotal += t.getAmount();
+            else
+                receiveTotal += t.getAmount();
+        }
+
+        receiveTotalText.setText(String.format("%.2f", receiveTotal));
+        debtTotalText.setText(String.format("%.2f", debtTotal));
     }
 
     public void onDestroy() {
